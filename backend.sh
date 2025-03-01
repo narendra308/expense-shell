908b76817fcc5b79eadad01acb47e2086cf5cb3d
narendramogili <narendramogili308@gmail.com>
2025-03-01 12:31:26 +0530

    expense

--- backend.sh
+++ backend.sh
@@ -43,7 +43,7 @@ VALIDATE $? "Installing Nodejs"
 useradd expense &>>$LOG_FILE_NAME
 VALIDATE $? "Adding expense user"
 
-mkdir /app &>>$LOG_FILE_NAME
+mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creacting app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
