60ca67cea9cf90d55febb5caea1575389d7d47f9
narendramogili <narendramogili308@gmail.com>
2025-03-04 20:56:58 +0530

    expense

--- frontend.sh
+++ frontend.sh
@@ -30,5 +30,33 @@ CHEK_ROOT(){
     fi
 }
 
+        mkdir -p $LOGS_FOLDER
             echo " script starting and execucting at: $TIMESTAMP"
 CHEK_ROOT
+
+dnf install nginx -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Nginx server"
+
+systemctl enable nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Ngina server"
+
+systemctl start nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Nginx server"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
+VALIDATE $? "Removing existing version of code"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading latest code"
+
+cd /usr/share/nginx/html  &>>$LOG_FILE_NAME
+VALIDATE $? " Moving to HTML directory"
+
+unzip /tmp/frontend.zip  &>>$LOG_FILE_NAME
+VALIDATE $? "Unzipping the frontend code"
+
+systemctl restart nginx  &>>$LOG_FILE_NAME
+VALIDATE $? "Restart Nginx server"
+
+
+
